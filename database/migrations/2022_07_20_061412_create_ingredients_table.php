c725001a6b5a33ed4396139850e9547c0d75eb93
luis <luisprg05@gmail.com>
2022-07-21 18:55:25 -0500

    Creada base del proyecto con laravel

---
+++ database/migrations/2022_07_20_061412_create_ingredients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->integer('calories');
+            $table->boolean('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+};
